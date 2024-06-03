d6a601a4cc5bcfb28a4e501a7e142b2ed3d49c41
Kristina Kovach <kristina.kovach@team.wrike.com>
2024-06-03 19:42:13 +0300

    hw-2: add tabs

--- src/components/restaurants/component.jsx
+++ src/components/restaurants/component.jsx
@@ -1,20 +1,33 @@
 import { restaurants } from "../../constants/mock.js";
 import { Restaurant } from "../restaurant/component.jsx";
+import { useState } from "react";
 
 export const Restaurants = () => {
-  if (restaurants.length === 0) {
+  if (restaurants === null || restaurants.length === 0) {
     return (
       <div>Oops, no restaurants available right now</div>
     )
   }
 
+  const [selectedRestaurant, setSelectedRestaurant] = useState(restaurants[0]);
+
+  const handleRestaurantTabClick = (event) => {
+    const restaurantId = event.target?.id;
+    const restaurant = restaurants.find((restaurant) => restaurant.id === restaurantId);
+    setSelectedRestaurant(restaurant);
+  };
+
   return (
     <div>
       {restaurants.map((restaurant) => (
-        <Restaurant
-          restaurant={restaurant}
-        />
+        <button id={restaurant?.id} onClick={handleRestaurantTabClick}>{restaurant?.name}</button>
       ))}
+
+      {selectedRestaurant && (
+        <Restaurant
+          restaurant={selectedRestaurant}
+        />
+      )}
     </div>
   );
 };
