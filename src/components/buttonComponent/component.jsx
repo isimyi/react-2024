9368ea37c7b18d91252ca2f3cfaaf29bb3c8c41d
Kristina Kovach <kristina.kovach@team.wrike.com>
2024-06-13 11:52:22 +0300

    hw-5: fix theme switch

--- src/components/buttonComponent/component.jsx
+++ src/components/buttonComponent/component.jsx
@@ -2,15 +2,15 @@ import { useContext } from "react";
 import { ThemeContext } from "../../App.jsx";
 
 export const ButtonComponent = ({ children, onClick, isDisabled }) => {
-  const theme = useContext(ThemeContext);
+  const { theme } = useContext(ThemeContext);
 
   return (
     <button
       disabled={isDisabled}
       onClick={onClick}
-      style={theme === 'dark' ?
-        { backgroundColor: "midnightblue", color: "white" } :
-        { backgroundColor: "lightskyblue", color: "darkslategrey" }}>
+      style={theme === "light" ?
+        { backgroundColor: "lightskyblue", color: "darkslategrey" } :
+        { backgroundColor: "midnightblue", color: "white" }}>
       {children}
     </button>
   )
